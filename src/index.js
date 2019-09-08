5a62fe8ae11e0eec674657e09cd20a94a5c27368
Sergei <se-panfilov@ya.ru>
2019-09-08 15:35:49 +0200

    case 1

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ console.log('It works!');
 
 // YOUR CODE HERE
 
+// 1. Print numbers from 1 to 100 to the console, but for each number also print a random word using the function `getRandomWordSync`. E.g.
+console.log(...[...new Array(100)].map((v, i) => i + 1 + ': ' + getRandomWordSync()))
+
