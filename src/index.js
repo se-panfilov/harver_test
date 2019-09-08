4b78954b00896182a67bf1c4e2e6dbed5b254c8c
Sergei <se-panfilov@ya.ru>
2019-09-08 15:52:12 +0200

    case 3

--- src/index.js
+++ src/index.js
@@ -29,3 +29,34 @@ function fuzzerBuzzer(num, str) {
 }
 
 console.log(...result)
+
+// 3. Create a version of steps *1* and *2* using the **asynchronous** function, `getRandomWord`. This function
+// returns a Promise, which resolves to a random word string. The numbers may or may not be in numerical order.
+
+// async version of case 1
+const result = [...new Array(100)].map(async (v, i) => i + 1 + ': ' + await getRandomWord())
+Promise.all(result).then(() => {
+    return console.log(...result);
+})
+
+// async version of case 2
+const result = [...new Array(100)].map(async (v, i) => await getValue(i))
+
+async function getValue(numVal) {
+    const num = numVal + 1
+    const word = await getRandomWord()
+    return num + ': ' + fuzzerBuzzer(num, word)
+}
+
+function fuzzerBuzzer(num, str) {
+    const isFuzz = (num % 3) === 0
+    const isBuzz = (num % 5) === 0
+
+    if (!isFuzz && !isBuzz) return str
+
+    let result = ''
+    if (isFuzz) result += 'Fuzz'
+    if (isBuzz) result += 'Buzz'
+    return result
+}
+Promise.all(result).then(() => console.log(...result));
