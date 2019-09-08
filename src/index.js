591e9d4172145fc88bc7cf210a571b3c695bea59
Sergei <se-panfilov@ya.ru>
2019-09-08 16:43:09 +0200

    common fixes

--- src/index.js
+++ src/index.js
@@ -4,24 +4,28 @@ console.log('It works!');
 
 // YOUR CODE HERE
 
+const numberList = [...new Array(100)]
+
 // 1. Print numbers from 1 to 100 to the console, but for each number also print a random word using the function `getRandomWordSync`. E.g.
 function case1Sync() {
-    console.log(...[...new Array(100)].map((v, i) => i + 1 + ': ' + getRandomWordSync()))
+    console.log(...numberList.map((v, i) => i + 1 + ': ' + getRandomWordSync() + '\n'))
 }
 
+console.info('Case 1:\n')
 // case1Sync()
+console.info('-----------\n')
 
 // 2. Modify your code to be a "Fizz Buzz" program. That is, print the numbers as in the previous step, but
 // for multiples of three, print "Fizz" (instead of the random word), for multiples of five, print "Buzz" and
 // for numbers which are both multiples of three and five, print "FizzBuzz".
 
 function case2SyncFuzzBuzz() {
-    const result = [...new Array(100)].map((v, i) => {
+    const result = numberList.map((v, i) => {
         const num = i + 1
         return num + ': ' + fuzzerBuzzer(num, getRandomWordSync())
     })
 
-    console.log(...result)
+    console.log(...result.map(v => v + '\n'))
 }
 
 function fuzzerBuzzer(num, str) {
@@ -36,23 +40,30 @@ function fuzzerBuzzer(num, str) {
     return result
 }
 
+console.info('Case 2:\n')
 // case2SyncFuzzBuzz()
+console.info('-----------\n')
 
 // 3. Create a version of steps *1* and *2* using the **asynchronous** function, `getRandomWord`. This function
 // returns a Promise, which resolves to a random word string. The numbers may or may not be in numerical order.
 
 // async version of case 1
 function case3Async() {
-    const result = [...new Array(100)].map(async (v, i) => i + 1 + ': ' + await getRandomWord())
-    Promise.all(result).then(() => console.log(...result))
+    const result = numberList.map(async (v, i) => i + 1 + ': ' + await getRandomWord())
+
+    Promise.all(result).then((resolved) => {
+        console.info('Case 3a:\n')
+        console.log(...resolved.map(v => v + '\n'))
+        console.info('-----------\n')
+    })
 }
+
 
 // case3Async()
 
-
 // async version of case 2
 function case3AsyncFuzzBuzz() {
-    const result = [...new Array(100)].map(async (v, i) => await getValue(i))
+    const result = numberList.map(async (v, i) => await getValue(i))
 
     async function getValue(numVal) {
         const num = numVal + 1
@@ -60,7 +71,13 @@ function case3AsyncFuzzBuzz() {
         return num + ': ' + fuzzerBuzzer(num, word)
     }
 
-    Promise.all(result).then(() => console.log(...result));
+
+    Promise.all(result).then(resolved => {
+        console.info('Case 3b:\n')
+        console.log(...resolved.map(v => v + '\n'))
+        console.info('-----------\n')
+    });
+
 }
 
 // case3AsyncFuzzBuzz()
@@ -72,28 +89,36 @@ function case3AsyncFuzzBuzz() {
 
 // async version of case 1
 function case4AsyncWithErrors() {
-    const result = [...new Array(100)]
+    const result = numberList
         .map(async (v, i) => {
-            return i + 1 + ': ' + await getRandomWord({ withErrors: true }).catch(err => `It shouldn't break anything!`);
+            return i + 1 + ': ' + await getRandomWord({ withErrors: true }).catch(err => `${num}: It shouldn't break anything!`);
         })
 
-    Promise.all(result).then(() => console.log(...result))
+    Promise.all(result).then(resolved => {
+        console.info('Case 4a:\n')
+        console.log(...resolved.map(v => v + '\n'))
+        console.info('-----------\n')
+    })
 }
 
 // case4AsyncWithErrors()
 
 // async version of case 2
 function case4AsyncFuzzBuzzWithErrors() {
-    const result = [...new Array(100)].map(async (v, i) => await getValue(i))
+    const result = numberList.map(async (v, i) => await getValue(i))
 
     async function getValue(numVal) {
         const num = numVal + 1
-        return await getRandomWord({ withErrors: true })
+        return getRandomWord({ withErrors: true })
             .then(word => num + ': ' + fuzzerBuzzer(num, word))
-            .catch(() => `It shouldn't break anything!`)
+            .catch(() => `${num}: It shouldn't break anything!`)
     }
 
-    Promise.all(result).then(() => console.log(...result));
+    Promise.all(result).then(resolved => {
+        console.info('Case 4b:\n')
+        console.log(...resolved.map(v => v + '\n'))
+        console.info('-----------\n')
+    });
 }
 
 case4AsyncFuzzBuzzWithErrors()
